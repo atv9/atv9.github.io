a634747f732835542e256accf872f0d2c89b454a
atv9 <157957429+atv9@users.noreply.github.com>
2026-08-19 03:54:07 +0400

    Add files via upload

--- userscripts.js
+++ userscripts.js
@@ -1,7 +1,7 @@
 /* Start spoofViewport.js */
 // Enables 4K resolution tricking youtube into thinking that we are on a 4K TV
 (function () {
-  if (window.screen.width >= 1920 || window.screen.height >= 1080) return;
+  if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
 
   var existing = document.querySelector('meta[name="viewport"]');
   if (existing) {
@@ -12,7 +12,7 @@
   } else {
     var meta = document.createElement("meta");
     meta.name = "viewport";
-    meta.content = "width=3840, height=2160, initial-scale=1.0";
+    meta.content = "width=1920, height=1080, initial-scale=2.0";
     document.head.appendChild(meta);
   }
 })();
